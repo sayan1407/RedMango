bf76e149edb8da8f668269d40a29e8982bc133e0
Sayan Saha <sayan199783@gmail.com>
2025-03-24 08:57:54 +0530

    Fetch menuitems

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Footer, Header } from '../Components/Layout';
-
+import { useState,useEffect } from 'react';
+import { menuItemModel } from '../Interface';
 
 
 function App() {
+  const [menuItems,setMenuItems] = useState<menuItemModel[]>([])
+  useEffect(() => {
+    fetch("https://localhost:44307/api/MenuItem")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setMenuItems(data);
+    })
+  },[])
   return (
     <div className="text-primary">
        <Header/>
