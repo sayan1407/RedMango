e5129b422ac8428d32bd93ff193fd70baab4391d
Sayan Saha <sayan199783@gmail.com>
2025-03-30 22:29:01 +0530

    Load menu item details

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useGetMenuItemByIdQuery } from '../Apis/MenuItemApi';
 
 function MenuItemDetails() {
+  const {menuItemId} = useParams();
+  const {data,isLoading} = useGetMenuItemByIdQuery(menuItemId);
+  const navigate = useNavigate();
+  if(isLoading)
+  {
+    return(
+      <div>Loading...</div>
+    )
+  }
+  else{
+    console.log(data)
+    let imageUrl = require("../Assets/Images/".concat(data.result.image))
   return (
     <div className="container pt-4 pt-md-5">
     <div className="row">
       <div className="col-7">
-        <h2 className="text-success">NAME</h2>
+        <h2 className="text-success">{data.result.name}</h2>
         <span>
           <span
             className="badge text-bg-dark pt-2"
             style={{ height: "40px", fontSize: "20px" }}
           >
-            CATEGORY
+            {data.result.category}
           </span>
         </span>
         <span>
@@ -19,13 +33,13 @@ function MenuItemDetails() {
             className="badge text-bg-light pt-2"
             style={{ height: "40px", fontSize: "20px" }}
           >
-            SPECIAL TAG
+            {data.result.specialTag}
           </span>
         </span>
         <p style={{ fontSize: "20px" }} className="pt-2">
-          DESCRIPTION
+          {data.result.description}
         </p>
-        <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
+        <span className="h3">{data.result.price}</span> &nbsp;&nbsp;&nbsp;
         <span
           className="pb-2  p-3"
           style={{ border: "1px solid #333", borderRadius: "30px" }}
@@ -48,7 +62,8 @@ function MenuItemDetails() {
           </div>
 
           <div className="col-5 ">
-            <button className="btn btn-secondary form-control">
+            <button className="btn btn-secondary form-control"
+            onClick={() => navigate(-1)}>
               Back to Home
             </button>
           </div>
@@ -56,7 +71,7 @@ function MenuItemDetails() {
       </div>
       <div className="col-5">
         <img
-          src="https://via.placeholder.com/150"
+          src={imageUrl}
           width="100%"
           style={{ borderRadius: "50%" }}
           alt="No content"
@@ -65,6 +80,7 @@ function MenuItemDetails() {
     </div>
   </div>
   )
+}
 }
 
 export default MenuItemDetails
