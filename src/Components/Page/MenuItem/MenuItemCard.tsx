4e601a9748fc87a4ab14e3d503282efdce6e9f43
Sayan Saha <sayan199783@gmail.com>
2025-04-07 14:56:18 +0530

    Mini Loader Component

--- src/Components/Page/MenuItem/MenuItemCard.tsx
+++ src/Components/Page/MenuItem/MenuItemCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { menuItemModel } from "../../../Interface";
 import { Link } from "react-router-dom";
 import { useUpdateShoppingCartMutation } from "../../../Apis/ShoppingCartApi";
+import { MiniLoader } from "../Common";
 
 interface Props {
   menuItem: menuItemModel;
@@ -65,10 +66,7 @@ function MenuItemCard(props: Props) {
                 right: "15px",
               }}
             >
-              <div
-                className={"spinner-border text-warning"}
-                style={{ scale: "100%" }}
-              ></div>
+              <MiniLoader />
             </div>
           ) : (
             <i
