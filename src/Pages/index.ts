627d2659e3cd71dce17ce31836f1d689eea79b55
Sayan Saha <sayan199783@gmail.com>
2025-04-13 16:37:16 +0530

    Register and Login UI

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,5 +2,7 @@ import Home from "./Home"
 import NotFound from "./NotFound"
 import MenuItemDetails from "./MenuItemDetails"
 import ShoppingCart from "./ShoppingCart"
+import Login from "./Login"
+import Register from "./Register"
 
-export {Home,NotFound,MenuItemDetails,ShoppingCart}
+export {Home,NotFound,MenuItemDetails,ShoppingCart,Login,Register}
--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Footer, Header } from "../Components/Layout";
 import { Routes, Route } from "react-router-dom";
-import { Home, MenuItemDetails, NotFound, ShoppingCart } from "../Pages";
+import { Home, Login, MenuItemDetails, NotFound, Register, ShoppingCart } from "../Pages";
 import { useDispatch } from "react-redux";
 import { useGetShoppingCartQuery } from "../Apis/ShoppingCartApi";
 import { setCartItem } from "../Storage/Redux/shoppingCartSlice";
@@ -29,6 +29,8 @@ function App() {
             element={<MenuItemDetails />}
           ></Route>
           <Route path="/shoppingCart" element={<ShoppingCart/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
         </Routes>
       </div>
 
