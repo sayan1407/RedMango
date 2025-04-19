732a9fb787c4b54d5c50302a66d8293f3da9141d
Sayan Saha <sayan199783@gmail.com>
2025-04-19 19:47:49 +0530

    Login in action

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,21 +1,45 @@
 import React, { useState } from 'react'
 import { inputHelper } from '../Helper';
+import { useLoginUserMutation } from '../Apis/AuthApi ';
+import { apiResponse } from '../Interface';
 
 function Login() {
+  const [loginUser] = useLoginUserMutation();
+  const[error,setError] = useState("");
   const[loading,setLoading] = useState(false);
   const[userInput,setUserInput] = useState({
-    name : "",
+ 
     userName : "",
-    password : "",
-    role : ""
+    password : ""
+   
   })
   const handleUSerInput = (e : React.ChangeEvent<HTMLInputElement  | HTMLSelectElement>) => {
     const tempData = inputHelper(e,userInput);
     setUserInput(tempData);
   }
+  const handleSubmit = async (e : React.ChangeEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setLoading(true);
+      const response : apiResponse = await loginUser({
+        userName : userInput.userName,
+        password : userInput.password,
+      });
+      console.log(response);
+
+      if(response.data)
+      {
+          console.log(response);
+      }
+      else if(response.error)
+      {
+        console.log(response.error.data!.errorMessages![0])
+        setError(response.error.data!.errorMessages![0])
+      }
+      setLoading(false);
+    }
   return (
     <div className="container text-center">
-    <form method="post">
+    <form method="post" onSubmit={handleSubmit}>
       <h1 className="mt-5">Login</h1>
       <div className="mt-5">
         <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
