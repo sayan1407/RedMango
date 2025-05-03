00c79539c7a669cbba1dce74259774c4d39572e1
Sayan Saha <sayan199783@gmail.com>
2025-05-03 16:17:35 +0530

    Create Payment API

---
+++ src/Apis/PaymentApi.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+ const paymentAPI = createApi({
+  reducerPath: "paymentAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:44307/api/" }),
+  tagTypes : ["Payments"],
+  endpoints: (builder) => ({
+    //QUERY -> GET
+    //MUTATION -> POST/PUT/DELETE
+    initiatePayment: builder.mutation({
+      query: (userID) => ({
+        url : "Payment",
+        method : "POST",
+        params : {
+          userId : userID
+        }
+      }),
+      
+    })
+   
+   
+  }),
+});
+export const { useInitiatePaymentMutation
+ } = paymentAPI;
+
+ export default paymentAPI;
--- src/Apis/index.ts
+++ src/Apis/index.ts
@@ -1,5 +1,6 @@
 import menuItemAPI from "./MenuItemApi";
 import shoppingCartAPI from "./ShoppingCartApi";
 import authAPI from "./AuthApi ";
+import paymentAPI from "./PaymentApi";
 
-export {menuItemAPI,shoppingCartAPI,authAPI};
+export {menuItemAPI,shoppingCartAPI,authAPI,paymentAPI};
