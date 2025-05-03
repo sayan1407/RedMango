9b84e8410ad658ae75c942b17fb0423e5889e439
Sayan Saha <sayan199783@gmail.com>
2025-05-03 17:28:54 +0530

    Stripe card component

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import { PaymentForm } from '../Components/Page/Payment';
+
 
 function Payment() {
   const {
     state : {apiResult, inputData,orderSummary}
   } = useLocation();
   console.log(apiResult);
+  const stripePromise = loadStripe('pk_test_51NQ2MqSEMQXGUvnj9f0JFvtNHIHXdIQxqQbSRxQfp78ggsHL8rnfOUA1XmKPF7NGh4tim3zvUIHN1RClIVzLUEIC000bVIF2R5');
+  const options = {
+    clientSecret : apiResult.clientSecret
+  }
   return (
-    <div>Payment</div>
+    <Elements stripe={stripePromise} options={options}>
+    <PaymentForm />
+  </Elements>
   )
 }
 
